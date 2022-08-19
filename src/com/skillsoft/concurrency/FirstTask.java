ffef55eafea320537f9d7039b4fd8a91e44c672b
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-19 23:33:28 +0200

    ICTTrainingen : The tryLock method.

--- src/com/skillsoft/concurrency/FirstTask.java
+++ src/com/skillsoft/concurrency/FirstTask.java
@@ -13,20 +13,24 @@ public class FirstTask implements Runnable {
     @Override
     public void run() {
         try {
-            rOne.rOneLock.lock();
-            System.out.println("Lock acquired on ResourceOne by " + Thread.currentThread().getName());
-            rOne.myVar++;
-            Thread.sleep(1000);
-            rOne.rOneLock.unlock();
-            System.out.println("Lock on ResourceOne released by " + Thread.currentThread().getName());
 
-            rTwo.rTwoLock.lock();
-            System.out.println("Lock acquired on ResourceTwo by " + Thread.currentThread().getName());
-            rTwo.myVar--;
-            Thread.sleep(1000);
-            rTwo.rTwoLock.unlock();
-            System.out.println("Lock on ResourceTwo released by " + Thread.currentThread().getName());
-
+            boolean rOneLockAcquired = rOne.rOneLock.tryLock();
+            if (rOneLockAcquired){
+                System.out.println("Lock acquired on ResourceOne by " + Thread.currentThread().getName());
+                rOne.myVar++;
+                Thread.sleep(1000);
+                rOne.rOneLock.unlock();
+                System.out.println("Lock on ResourceOne released by " + Thread.currentThread().getName());
+            }
+            boolean rTwoLockAcquired = rTwo.rTwoLock.tryLock();
+            if (rTwoLockAcquired){
+                rTwo.rTwoLock.lock();
+                System.out.println("Lock acquired on ResourceTwo by " + Thread.currentThread().getName());
+                rTwo.myVar--;
+                Thread.sleep(1000);
+                rTwo.rTwoLock.unlock();
+                System.out.println("Lock on ResourceTwo released by " + Thread.currentThread().getName());
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/com/skillsoft/concurrency/SecondTask.java
+++ src/com/skillsoft/concurrency/SecondTask.java
@@ -1,6 +1,6 @@
 package com.skillsoft.concurrency;
 
-public class SecondTask implements Runnable{
+public class SecondTask implements Runnable {
 
     ResourceOne rOne;
     ResourceTwo rTwo;
@@ -13,21 +13,23 @@ public class SecondTask implements Runnable{
     @Override
     public void run() {
         try {
-            rTwo.rTwoLock.lock();
-            System.out.println("Lock acquired on ResourceTwo by " + Thread.currentThread().getName());
-            rTwo.myVar++;
-            Thread.sleep(1000);
-            rTwo.rTwoLock.unlock();
-            System.out.println("Lock on ResourceTwo released by " + Thread.currentThread().getName());
-
-
-            rOne.rOneLock.lock();
-            System.out.println("Lock acquired on ResourceOne by " + Thread.currentThread().getName());
-            rOne.myVar--;
-            Thread.sleep(1000);
-            rOne.rOneLock.unlock();
-            System.out.println("Lock on ResourceOne released by " + Thread.currentThread().getName());
-
+            boolean rTwoLockAcquired = rTwo.rTwoLock.tryLock();
+            if (rTwoLockAcquired) {
+                rTwo.rTwoLock.lock();
+                System.out.println("Lock acquired on ResourceTwo by " + Thread.currentThread().getName());
+                rTwo.myVar++;
+                Thread.sleep(5000);
+                rTwo.rTwoLock.unlock();
+                System.out.println("Lock on ResourceTwo released by " + Thread.currentThread().getName());
+            }
+            boolean rOneLockAcquired = rOne.rOneLock.tryLock();
+            if (rOneLockAcquired) {
+                System.out.println("Lock acquired on ResourceOne by " + Thread.currentThread().getName());
+                rOne.myVar--;
+                Thread.sleep(1000);
+                rOne.rOneLock.unlock();
+                System.out.println("Lock on ResourceOne released by " + Thread.currentThread().getName());
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
