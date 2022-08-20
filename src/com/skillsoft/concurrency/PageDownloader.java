f03862e2a828b6704afcac1c92ebc77473df1368
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-20 17:11:22 +0200

    Icttrainingen : The Future Object.

--- src/com/skillsoft/concurrency/PageDownloader.java
+++ src/com/skillsoft/concurrency/PageDownloader.java
@@ -5,17 +5,16 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
 
 public class PageDownloader implements Runnable {
     String[] urlsList;
-    CountDownLatch latch;
 
-    public PageDownloader(String[] urlsList, CountDownLatch latch) {
+    public PageDownloader(String[] urlsList) {
         this.urlsList = urlsList;
-        this.latch = latch;
     }
 
     @Override
@@ -23,16 +22,11 @@ public class PageDownloader implements Runnable {
         String threadName = Thread.currentThread().getName();
 
         try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(threadName + " has been interrupted before downloading " + urlsList[0]);
-        }
-
-        try {
             for (String urlString : urlsList) {
                 if (Thread.currentThread().isInterrupted()) {
                     throw new InterruptedException(threadName + " is interrupted");
                 }
+
                 URL url = new URL(urlString);
                 String fileName = "files/" + urlString.substring(urlString.lastIndexOf('/') + 1).trim() + ".html";
                 BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -48,25 +42,41 @@ public class PageDownloader implements Runnable {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        latch.countDown();
     }
 
     public static void main(String[] args) throws InterruptedException {
         String[] urlsList = {"https://tracefy.com/nl", "https://tracefy.com/nl/b2b", "https://tracefy.com/nl/b2b/fleetmanagement", "https://tracefy.com/nl/b2b/deelvoertuigen-en-verhuur", "https://tracefy.com/nl/b2b/delivery", "https://tracefy.com/nl/b2b/fietsmerken-oem", "https://tracefy.com/nl/consumer", "https://tracefy.com/nl/diefstal-opsporing", "https://tracefy.com/nl/consumer/dealer-worden", "https://tracefy.com/nl/consumer/dealer-zoeken", "https://tracefy.com/nl/elektrische-fiets-verzekeren-met-tracefy", "https://tracefy.com/nl/over-tracefy", "https://tracefy.com/nl/over-tracefy/tracefy-team", "https://tracefy.com/nl/over-tracefy/werken-bij", "https://tracefy.com/nl/over-tracefy/media", "https://tracefy.com/nl/contact"};
 
-        int maxThreads = 4;
-        CountDownLatch latch = new CountDownLatch(maxThreads);
-        ExecutorService executorService = Executors.newFixedThreadPool(maxThreads);
+        Thread downloaderOne = new Thread(new PageDownloader(Arrays.copyOfRange(urlsList, 0, urlsList.length / 2)));
+        Thread downloaderTwo = new Thread(new PageDownloader(Arrays.copyOfRange(urlsList, urlsList.length / 2, urlsList.length)));
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         long startTime = System.currentTimeMillis();
 
-        for (String url : urlsList) {
-            Thread downloader = new Thread(new PageDownloader(new String[]{url}, latch));
-            executorService.submit(downloader);
+        Future<?> fOne = executorService.submit(downloaderOne);
+        Future<?> fTwo = executorService.submit(downloaderTwo);
+
+        int checkCount = 0;
+
+        while (true) {
+            checkCount++;
+
+            if (checkCount > 3){
+                fOne.cancel(true);
+                fTwo.cancel(true);
+                System.out.println("The downloaders have been CANCELED!");
+                break;
+            }
+            if (fOne.isDone() && fTwo.isDone()) {
+                System.out.println("The downloaders are DONE!");
+                break;
+            }
+            System.out.println("Check #" + checkCount + " Downloaders is still on...");
+            Thread.sleep(2000);
         }
 
-        latch.await();
-        executorService.shutdownNow();
+        executorService.shutdown();
+
         while (!executorService.isTerminated()) {
             Thread.sleep(1000);
         }
