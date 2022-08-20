131bad1cb3e95226cb3f346ab904cd3558dfbdbe
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-20 13:18:57 +0200

    Icttrainingen : Permit acquisition in semaphores.

--- src/com/skillsoft/concurrency/MyTask.java
+++ src/com/skillsoft/concurrency/MyTask.java
@@ -1,31 +1,34 @@
 package com.skillsoft.concurrency;
 
 import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 public class MyTask implements Runnable {
 
     SharedResource sr;
     Semaphore sem;
     String threadName;
+    int numPermits = 1;
 
-    public MyTask(SharedResource sr, Semaphore sem) {
+    public MyTask(SharedResource sr, Semaphore sem, int permits) {
         this.sr = sr;
         this.sem = sem;
+        this.numPermits = permits;
     }
 
     @Override
     public void run() {
         try {
             threadName = Thread.currentThread().getName();
-            System.out.println(threadName + " is waiting for the semaphore...");
-            sem.acquire();
-            System.out.println(threadName + " has ACQUIRED the semaphore! ");
+            System.out.println(threadName + " is waiting for " + numPermits + " the semaphore...");
+            sem.acquire(numPermits);
+            System.out.println(threadName + " has ACQUIRED " + numPermits + " permits.");
 
+            // Do work
             Thread.sleep((long) (Math.random() * 1000) * 5);
-            int numListElements = sr.myList.size();
 
-            sem.release();
-            System.out.println(threadName + " has RELEASED the semaphore! ");
+            sem.release(numPermits);
+            System.out.println(threadName + " has RELEASED " + numPermits + " permits!");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -33,10 +36,13 @@ public class MyTask implements Runnable {
 
     public static void main(String[] args) {
         SharedResource sharedRes = new SharedResource();
-        Semaphore sem = new Semaphore(4);
+        int maxPermits = 4;
+        Semaphore sem = new Semaphore(maxPermits);
 
+        Random random = new Random();
         for (int i = 0; i < 10; i++) {
-            Thread t = new Thread(new MyTask(sharedRes, sem), "Thread " + i);
+            int permits = random.nextInt(maxPermits) + 1;
+            Thread t = new Thread(new MyTask(sharedRes, sem, permits), "Task-" + i);
             t.start();
         }
     }
